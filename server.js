49b133e4c150659e7fe8d3cdbe3c5b8f4560dfba
Ken G <munky266@gmail.com>
2021-05-27 14:06:20 -0700

    Completed project

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 // Setup empty JS object to act as endpoint for all routes
-projectData = {};
+const projectData = {};
 
+const recentEntries = [];
 // Require Express to run server and routes
-
+const express = require('express');
+const bodyParser = require('body-parser');
 // Start up an instance of app
+const app = express();
 
+const cors = require('cors');
+const { builtinModules } = require('module');
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -12,8 +17,35 @@ app.use(bodyParser.json());
 
 // Cors for cross origin allowance
 
+app.use(cors());
 // Initialize the main project folder
 app.use(express.static('website'));
 
+const port = 3000;
 
 // Setup Server
+
+const server = app.listen(port, () => console.log(`listening on port ${port}`));
+
+//setup post route
+
+app.post( '/addEntry', function(req,res){
+
+    let convertTemp =  (req.body.temperature - 273.15) * 9/5 + 32;
+
+  let newEntry = {
+    temperature:  convertTemp.toPrecision(4),
+    currentDate: req.body.newDate,
+    content: req.body.userfeelings,
+  }
+  console.log(newEntry);
+
+  recentEntries.push(newEntry);
+  
+})
+
+
+app.get('/all', function(req,res){
+
+  res.send(recentEntries);
+})
--- website/app.js
+++ website/app.js
@@ -1,5 +1,111 @@
 /* Global Variables */
 
+// const { json } = require("body-parser");
+
+const baseURL= 'http://api.openweathermap.org/data/2.5/weather?zip=';
+const apiKey = ',us&appid=408281d3f29e387234edd3843c3b0e04';
+
 // Create a new date instance dynamically with JS
-let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+const entryHistory = document.querySelector('#entryHolder');
+
+
+document.getElementById('generate').addEventListener('click',performAction );
+
+
+function performAction(e){
+
+    const zipCode = document.getElementById('zip').value;
+    const userFeelings = document.getElementById('feelings').value;
+    let d = new Date();
+    let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+    getData(baseURL, zipCode, apiKey)
+    .then(function(newData){
+            postData( '/addEntry', {temperature: newData.main.temp, userfeelings:userFeelings, newDate : newDate})
+
+            updateUI();
+        }
+    )
+}
+//create get function
+
+const getData = async (baseURL,zip, key) =>{
+
+        const response = await fetch(baseURL+zip+key)
+        try{
+            const newData = await response.json();
+            return newData;
+
+        }catch(e){
+            console.log("Error Found", e);
+        }
+
+};
+
+
+//create post function
+
+const postData = async (url='', data = {}) => {
+
+    const responses = await fetch(url, {
+
+        method: 'POST', 
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    })
+
+    try{
+        const setData = await responses.json();
+
+    }catch(e){
+        console.log("Error", e);
+    }
+};
+
+
+const updateUI = async () => {
+    
+    const response = await fetch('/all')
+
+    try{
+        const allData = await response.json();
+        allData.forEach( element => {
+            
+
+            entryHistory.insertAdjacentHTML("afterend", `<div id = "entryHolders"><div id = "date">Today is ${element.currentDate}</div><div id = "temp">It's currently ${element.temperature}° in your area</div> <div id = "content">Your thoughts:<br> ${element.content}</div> </div> `);
+
+        })
+
+    }catch(e){
+        console.log("error", e)
+    }
+
+
+}
+
+
+const oldEntries = async () =>{
+
+    const response = await fetch('/all')
+
+    try{
+        const allData = await response.json();
+        allData.forEach( element => {
+            
+            entryHistory.insertAdjacentHTML("afterend", `<div id = "entryHolders"><div id = "date">Today is ${element.currentDate}</div><div id = "temp">It's currently ${element.temperature}° in your area</div> <div id = "content">Your thoughts:<br> ${element.content}</div> </div> `);
+
+
+
+        })
+
+    }catch(e){
+        console.log("error", e)
+    }
+};
+
+//call the function
+oldEntries();
